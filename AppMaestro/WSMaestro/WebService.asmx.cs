5fb953d4745fc02b037fbe1c82cfa36be08fd126
Ingrid Elizabeth Aguilar González <ingridaguilar741@gmail.com>
2021-06-06 00:40:23 -0600

    Skeleton Web Service

---
+++ AppMaestro/WSMaestro/WebService.asmx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Services;
+using System.Web.Services;
+
+namespace WSMaestro
+{
+    /// <summary>
+    /// Descripción breve de WebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    [System.Web.Script.Services.ScriptService]
+    public class WebService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        [ScriptMethod(UseHttpGet = true, ResponseFormat = ResponseFormat.Json)]
+        public string HelloWorld()
+        {
+            return "Hola a todos";
+        }
+    }
+}
